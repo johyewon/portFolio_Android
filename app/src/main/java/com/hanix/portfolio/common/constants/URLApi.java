a0d510452fb8aec7fe1c6e0a78d138a1fe4fc027
johyewon <ixxwxfjlwf@gmail.com>
2021-06-18 17:53:08 +0900

    init

--- app/src/main/java/com/hanix/portfolio/common/constants/URLApi.java
+++ app/src/main/java/com/hanix/portfolio/common/constants/URLApi.java
@@ -5,8 +5,7 @@ import com.hanix.portfolio.common.app.GlobalApplication;
 public class URLApi {
 
     // Server Url
-    private static final String URL_DEV_SERVER = "";
-    private static final String URL_REAL_SERVER = "";
+    public static final String URL_SERVER = "https://johyewon.github.io/";
 
     // 플레이 스토어 url
     public static final String PLAY_STORE_URL = AppConstants.STORE_URL_GOOGLE + "com.hanix.portfolio";
@@ -14,11 +13,7 @@ public class URLApi {
     /** Server url **/
     // url 취득
     public static String getServerURL() {
-        //Release 버전에서는 무조건 실서버를 바라본다
-        if(GlobalApplication.getInstance().isDebuggable) {
-            return URL_DEV_SERVER;
-        }
-        return URL_REAL_SERVER;
+        return URL_SERVER;
     }
 
     /** 플레이스토어 url **/
--- app/src/main/java/com/hanix/portfolio/view/AppIntro.java
+++ app/src/main/java/com/hanix/portfolio/view/AppIntro.java
@@ -2,7 +2,11 @@ package com.hanix.portfolio.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
 
 import com.hanix.portfolio.R;
 
@@ -12,5 +16,18 @@ public class AppIntro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_intro);
+        getIntent().addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(AppIntro.this, MainActivity.class));
+            overridePendingTransition(R.anim.fade_in_activity, R.anim.hold_activity);
+            finish();
+        }, 2000);
     }
+
+    @Override
+    public void onBackPressed() {
+    }
+
 }
